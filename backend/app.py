c39f0448523b185183f3b0f7b882688ca092e8ab
Sanika Mhatre <sanikamhatre2023@gmail.com>
2026-08-29 15:22:21 +0530

    Complete Nutrition Tracking API

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+from backend.routes.user_routes import user_bp
+from backend.routes.health_routes import health_bp
+from backend.routes.goal_routes import goal_bp
+from backend.routes.food_routes import food_bp
+from backend.routes.meal_routes import meal_bp
+from backend.routes.tracking_routes import tracking_bp
+from flask import Flask, jsonify
+from sqlalchemy import text
+
+from database.database import engine
+
+
+app = Flask(__name__)
+app.register_blueprint(user_bp)
+app.register_blueprint(health_bp)
+app.register_blueprint(goal_bp)
+app.register_blueprint(food_bp)
+app.register_blueprint(meal_bp)
+app.register_blueprint(tracking_bp)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Smart Diet & Nutrition Planner Backend is running!"
+    })
+
+
+@app.route("/health")
+def health():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+
+        return jsonify({
+            "status": "success",
+            "database": "connected"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "database": "not connected",
+            "message": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
